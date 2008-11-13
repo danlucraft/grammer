a08854b18e6605e0641ee296acb873f6b092ceff
Daniel Lucraft <dan@fluentradical.com>
2008-11-13 13:08:07 +0000

    Menu items work to open window and quit.

--- bin/grammer.rb
+++ bin/grammer.rb
@@ -8,12 +8,39 @@ class GrammerApplet < Gtk::StatusIcon
     self.stock = Gtk::Stock::ABOUT
     self.visible = true
 
+    menu = Gtk::Menu.new
+    item_open = Gtk::MenuItem.new("Open")
+    item_quit = Gtk::MenuItem.new("Quit")
+    item_open.signal_connect("activate") { open_window }
+    item_quit.signal_connect("activate") { Gtk.main_quit }
+    menu.append(item_open)
+    menu.append(item_quit)
+    menu.show_all
     signal_connect("activate") do
-      p :open_window
+      open_window
     end
 
-    signal_connect("popup-menu") do
-      p :popup_menu
+    signal_connect("popup-menu") do |_, button, time|
+      menu.popup(nil, nil, button, time)
+    end
+  end
+
+  def open_window
+    GrammerWindow.window ||= GrammerWindow.new
+    GrammerWindow.window.show_all
+  end
+end
+
+class GrammerWindow < Gtk::Window
+  class << self
+    attr_accessor :window
+  end
+
+  def initialize
+    super("Grammer")
+    signal_connect("destroy") do 
+      self.hide_all
+      GrammerWindow.window = nil
     end
   end
 end
